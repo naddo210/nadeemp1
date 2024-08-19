c5748efce9efc956f2b7197c39313fce9909e452
naddo210 <153388875+naddo210@users.noreply.github.com>
2024-08-19 22:10:02 +0530

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ import axios from "axios";
 export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://nadeemp1.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setfood_list] = useState([]);
   const addToCart = async (itemId) => {
